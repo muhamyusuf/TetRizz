faaa647db8bbd417bca886373e0905f67fbf12b8
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-05-20 18:50:03 +0700

    new set-up for dual player

---
+++ utils.py
@@ -0,0 +1,19 @@
+FONT = 'assets/font/helvetica-compressed.otf'
+
+import pygame
+
+def load_music():
+    pygame.mixer.music.load('assets/music/tetris-remix.ogg')
+
+def play_music():
+    pygame.mixer.music.play(-1)
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+def draw_text(surface, text, font, color, position):
+    text_obj = font.render(text, True, color)
+    surface.blit(text_obj, position)
+
+def load_image(path):
+    return pygame.image.load(path).convert_alpha()
