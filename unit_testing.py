19957f99172c8cd8b50c02c49f128e36fc3213f0
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-05-21 07:47:22 +0700

    add unit testing for fungsional on tetris game & settings

--- unit_testing.py
+++ unit_testing.py
@@ -2,6 +2,8 @@ import unittest
 import pygame
 from game import Menu, SettingsScreen, SoloTetrisGame, DualTetrisGame, GameOverScreen, PlayerWinScreen
 from settings import settings
+from utils import load_music, play_music, stop_music, draw_text, FONT
+from tetris import Tetris
 
 class TestMenu(unittest.TestCase):
     def setUp(self):
@@ -18,6 +20,7 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(self.menu.handle_event(event), 'settings')
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_x)
         self.assertEqual(self.menu.handle_event(event), 'menu')
+        print("TestMenu test_handle_event passed")
 
     def tearDown(self):
         pygame.quit()
@@ -27,6 +30,7 @@ class TestSettingsScreen(unittest.TestCase):
         pygame.init()
         self.screen = pygame.display.set_mode((1000, 700))
         self.settings_screen = SettingsScreen(self.screen)
+        load_music()  # Ensure music is loaded before tests
 
     def test_handle_event(self):
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_m)
@@ -41,6 +45,7 @@ class TestSettingsScreen(unittest.TestCase):
 
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_ESCAPE)
         self.assertEqual(self.settings_screen.handle_event(event), 'menu')
+        print("TestSettingsScreen test_handle_event passed")
 
     def tearDown(self):
         pygame.quit()
@@ -62,6 +67,7 @@ class TestSoloTetrisGame(unittest.TestCase):
         self.solo_game.handle_event(event)
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_SPACE)
         self.solo_game.handle_event(event)
+        print("TestSoloTetrisGame test_handle_event passed")
 
     def tearDown(self):
         pygame.quit()
@@ -83,7 +89,8 @@ class TestDualTetrisGame(unittest.TestCase):
         self.dual_game.handle_event(event)
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_LSHIFT)
         self.dual_game.handle_event(event)
-        
+        print("TestDualTetrisGame test_handle_event passed")
+
     def tearDown(self):
         pygame.quit()
 
@@ -96,6 +103,7 @@ class TestGameOverScreen(unittest.TestCase):
     def test_handle_event(self):
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_r)
         self.assertEqual(self.gameover_screen.handle_event(event), 'menu')
+        print("TestGameOverScreen test_handle_event passed")
 
     def tearDown(self):
         pygame.quit()
@@ -111,8 +119,76 @@ class TestPlayerWinScreen(unittest.TestCase):
         event = pygame.event.Event(pygame.KEYDOWN, key=pygame.K_r)
         self.assertEqual(self.player1_win_screen.handle_event(event), 'menu')
         self.assertEqual(self.player2_win_screen.handle_event(event), 'menu')
+        print("TestPlayerWinScreen test_handle_event passed")
 
     def tearDown(self):
+        pygame.quit()
+
+class TestSettings(unittest.TestCase):
+    def test_toggle_music(self):
+        initial_music_on = settings.music_on
+        settings.toggle_music()
+        self.assertNotEqual(settings.music_on, initial_music_on)
+        print("TestSettings test_toggle_music passed")
+
+    def test_set_theme(self):
+        initial_theme = settings.theme
+        new_theme = 'light' if initial_theme == 'default' else 'default'
+        settings.set_theme(new_theme)
+        self.assertEqual(settings.theme, new_theme)
+        print("TestSettings test_set_theme passed")
+
+class TestTetris(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 700))
+        self.tetris = Tetris(self.screen, {}, 0, 0, 0, 0)
+
+    def test_create_grid(self):
+        grid = self.tetris.create_grid()
+        self.assertEqual(len(grid), 20)
+        self.assertEqual(len(grid[0]), 10)
+        print("TestTetris test_create_grid passed")
+
+    def test_clear_lines(self):
+        self.tetris.grid = [[1]*10] + [[0]*10]*19
+        self.tetris.clear_lines()
+        self.assertEqual(self.tetris.grid, [[0]*10]*20)
+        print("TestTetris test_clear_lines passed")
+
+    def test_move_piece(self):
+        piece = self.tetris.current_piece
+        original_x = piece['x']
+        original_y = piece['y']
+        self.tetris.move_piece(1, 1)
+        self.assertEqual(piece['x'], original_x + 1)
+        self.assertEqual(piece['y'], original_y + 1)
+        print("TestTetris test_move_piece passed")
+
+    def tearDown(self):
+        pygame.quit()
+
+class TestUtils(unittest.TestCase):
+    def test_draw_text(self):
+        pygame.init()
+        screen = pygame.display.set_mode((1000, 700))
+        font = pygame.font.SysFont(FONT, 24)
+        draw_text(screen, "Test", font, (255, 255, 255), (50, 50))
+        print("TestUtils test_draw_text passed")
+        pygame.quit()
+
+    def test_play_music(self):
+        pygame.init()
+        load_music()
+        play_music()
+        # No assertion needed; if no exception is raised, the test is considered passed.
+        print("TestUtils test_play_music passed")
+
+    def test_stop_music(self):
+        pygame.init()
+        stop_music()
+        # No assertion needed; if no exception is raised, the test is considered passed.
+        print("TestUtils test_stop_music passed")
         pygame.quit()
 
 if __name__ == '__main__':
